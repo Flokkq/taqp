52d6a34a375b0904138904569ceddc5c4a28cacd
Flokkq <webcla21@htl-kaindorf.at>
2025-02-23 14:05:13 +0100

    feat(middleware): link rust middleware to core
    Signed-off-by: Flokkq <webcla21@htl-kaindorf.at>

--- core/build.zig
+++ core/build.zig
@@ -1,14 +1,8 @@
 const std = @import("std");
 const builtin = @import("builtin");
 
-fn linkDarwinDependencies(exe: *std.Build.Step.Compile) void {
-    exe.linkFramework("CoreAudio");
-    exe.linkFramework("AudioToolbox");
-}
-
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
-
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
@@ -25,12 +19,14 @@ pub fn build(b: *std.Build) void {
         else => @compileError("Unsupported OS"),
     }
 
+    const rust_lib = try setupMiddleware(b, optimize);
+    exe.addLibraryPath(rust_lib.dirname());
+    exe.linkSystemLibrary("middleware");
+
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
-
     run_cmd.step.dependOn(b.getInstallStep());
-
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
@@ -48,4 +44,47 @@ pub fn build(b: *std.Build) void {
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
+}
+
+fn linkDarwinDependencies(exe: *std.Build.Step.Compile) void {
+    exe.linkFramework("CoreAudio");
+    exe.linkFramework("AudioToolbox");
+}
+
+fn setupMiddleware(b: *std.Build, opt: std.builtin.OptimizeMode) !std.Build.LazyPath {
+    const base_path = b.path("../middleware");
+    const lib_name = "libmiddleware";
+
+    const tool_run = b.addSystemCommand(&.{"cargo"});
+    tool_run.setCwd(base_path);
+    tool_run.addArg("build");
+
+    var opt_path: []const u8 = undefined;
+    switch (opt) {
+        .ReleaseSafe, .ReleaseFast, .ReleaseSmall => {
+            tool_run.addArg("--release");
+            opt_path = "release";
+        },
+        .Debug => opt_path = "debug",
+    }
+
+    var lib_ext: []const u8 = "";
+    switch (builtin.os.tag) {
+        .windows => lib_ext = ".dll",
+        .macos => lib_ext = ".dylib",
+        .linux, .openbsd, .freebsd => lib_ext = ".so",
+        else => @compileError("Unsupported OS"),
+    }
+    const lib_filename = try std.mem.concat(b.allocator, u8, &.{ lib_name, lib_ext });
+
+    const generated = try b.allocator.create(std.Build.GeneratedFile);
+    generated.* = .{
+        .step = &tool_run.step,
+        .path = try std.fs.path.join(
+            b.allocator,
+            &.{ base_path.getPath(b), "target", opt_path, lib_filename },
+        ),
+    };
+
+    return std.Build.LazyPath{ .generated = .{ .file = generated } };
 }
---
+++ core/src/middleware.zig
@@ -0,0 +1,1 @@
+pub extern fn add(a: i32, b: i32) i32;
