87a56429be0616fd43f3b07f162862126dec9a5d
Flokkq <webcla21@htl-kaindorf.at>
2025-02-26 20:58:48 +0100

    feat(daemon): simple multithread to handle usb and socktets simoultaneously
    Signed-off-by: Flokkq <webcla21@htl-kaindorf.at>

--- daemon/src/main.zig
+++ daemon/src/main.zig
@@ -4,33 +4,32 @@ const net = std.net;
 
 const ipc = @import("ipc.zig");
 const usb = @import("usb.zig");
+const bindings = @import("bindings");
 
 pub fn main() !void {
+    const vendor_id: u16 = 0x10c4;
+    const product_id: u16 = 0xea60;
+    const usb_device = usb.connectToDevice(vendor_id, product_id) catch |err| {
+        std.log.err("Failed connecting to taqp device: {}", .{err});
+        return;
+    };
+    std.log.info("Found taqp device: {?}", .{usb_device});
+
+    const thread = try std.Thread.spawn(.{}, struct {
+        pub fn run(device: *bindings.UsbDevice) !void {
+            while (true) {
+                const message = usb.readFromDevice(device);
+
+                std.log.info("Recieved message from device: {any}", .{message});
+            }
+        }
+    }.run, .{usb_device});
+    thread.detach();
+
     const addr = net.Address.initIp4(.{ 127, 0, 0, 1 }, 18769);
     var server = try addr.listen(.{});
 
     std.log.info("listening at {}", .{server.listen_address});
-
-    const vendor_id: u16 = 0x10c4;
-    const product_id: u16 = 0xea60;
-    const device = usb.connectToDevice(vendor_id, product_id) catch |err| {
-        std.log.err("Failed connecting to taqp device: {}", .{err});
-        return;
-    };
-
-    std.log.info("Found taqp device: {?}", .{device});
-
-    usb.sendToDevice(device, .MuteVolume) catch |err| {
-        std.log.err("Failed sending data to device: {}", .{err});
-        return;
-    };
-
-    const message = usb.readFromDevice(device) catch |err| {
-        std.log.err("Failed reading data from device: {}", .{err});
-        return;
-    };
-
-    std.log.info("Recieved message from device: {}", .{message.tag});
 
     while (true) {
         const connection = try server.accept();
