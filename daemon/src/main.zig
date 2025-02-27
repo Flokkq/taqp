614b68a5dd2367d5dd580a29fb28798ec3903984
Flokkq <webcla21@htl-kaindorf.at>
2025-02-28 00:14:17 +0100

    feat(daemon): try reconnecting to device after connection loss
    Signed-off-by: Flokkq <webcla21@htl-kaindorf.at>

--- daemon/src/main.zig
+++ daemon/src/main.zig
@@ -12,28 +12,38 @@ pub fn main() !void {
     const vendor_id: u16 = 0x10c4;
     const product_id: u16 = 0xea60;
 
-    const usb_device = usb.connectToDevice(vendor_id, product_id) catch |err| {
-        std.log.err("Failed connecting to taqp device: {}", .{err});
-        return;
-    };
-    std.log.info("Found taqp device: {d}-{d}", .{ vendor_id, product_id });
-
     const thread = try std.Thread.spawn(.{}, struct {
-        pub fn run(device: *bindings.UsbDevice) !void {
+        pub fn run() !void {
             while (true) {
-                const message = usb.readFromDevice(device) catch |err| {
-                    std.log.err("Error reading from device: {}", .{err});
+                const device = usb.connectToDevice(vendor_id, product_id) catch |err| {
+                    std.log.err("Failed connecting to taqp device: {}", .{err});
+                    std.time.sleep(1_000_000_000);
                     continue;
                 };
 
-                std.log.info("Recieved message from device: {any}", .{message});
+                std.log.info("Found taqp device: {d}-{d}", .{ vendor_id, product_id });
 
-                usb.handleClient(message) catch |err| {
-                    std.log.err("Error processing message: {}", .{err});
-                };
+                while (true) {
+                    const message = usb.readFromDevice(device) catch |err| {
+                        std.log.err("Error reading from device: {}", .{err});
+
+                        // If we loose connection leave the inner loop to try
+                        // and poll a new connection.
+                        switch (err) {
+                            usb.UsbError.ConnectionLost => break,
+                            else => continue,
+                        }
+                    };
+
+                    std.log.info("Recieved message from device: {any}", .{message});
+
+                    usb.handleClient(message) catch |err| {
+                        std.log.err("Error processing message: {}", .{err});
+                    };
+                }
             }
         }
-    }.run, .{usb_device});
+    }.run, .{});
     thread.detach();
 
     const addr = net.Address.initIp4(.{ 127, 0, 0, 1 }, 18769);
